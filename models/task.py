9f08f143c7337dc156ccb7330310174f550f1e53
Tiago Yamaguti <tiagoyamaguti@protonmail.com>
2026-01-03 11:52:10 -0300

    Implementing CRUF of tasks and Updating .gitignore

---
+++ models/task.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, id, title, description, completed=False) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "title":self.title,
+            "description":self.description,
+            "completed":self.completed
+        }
+    
